3411cb5247a95ddb1a3c2891944f79ce60e28dd6
Marcus Linderoth <marcus@thingsquare.com>
2015-11-01 14:21:17 +0100

    updated examples

--- examples/set-volume.js
+++ examples/set-volume.js
@@ -9,16 +9,24 @@ if (args.length > 0) {
   }
 }
 
-console.log("setting volume to:" + vol);
-
+var retry_timeout = 10;
 var run_test = function() {
-  lgtv.connect(function(err, response){
-    if (!err) {
-      lgtv.set_volume(vol, function(err, response){
-        lgtv.disconnect();
-      }); // show float
-    }
-  }); // connect
-}; // run test
+    lgtv.discover_ip(retry_timeout, function(err, ipaddr) {
+      if (err) {
+        console.log("Failed to find TV IP address on the LAN. Verify that TV is on, and that you are on the same LAN/Wifi.");
+    
+      } else {
+        console.log("Found TV at address " + ipaddr + ", running example.");
+        lgtv.connect(ipaddr, function(err, response){
+          if (!err) {
+            console.log("setting volume to:" + vol);
+            lgtv.set_volume(vol, function(err, response){
+              lgtv.disconnect();
+            });
+          }
+        });
+      }
+    });
+};
 
 run_test();
